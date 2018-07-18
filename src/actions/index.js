68e3d37266e4a7db9edf05b7f6496d063a76a35f
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-07-18 12:46:54 -0400

    Add files via upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = '5855994eabfd68064e276ad9fa6d1e0f';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+export const FETCH_POSTS = 'fetch_posts';
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=RODPAULAEMILY';
 
-export function fetchWeather (city) {
-  const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
-
-  console.log('Request:', request);
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
-    type: FETCH_WEATHER,
+    type: FETCH_POSTS,
     payload: request
   };
 }
-
-// Fetch Weather is my action creator
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  weather: WeatherReducer
+  posts: PostsReducer
 });
 
 export default rootReducer;
